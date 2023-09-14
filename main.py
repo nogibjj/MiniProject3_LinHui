904ae4d42b0e1771df3be1f03dfe94dcc42a250c
Lin Hui <Lin@HaAir.lan>
2023-09-13 22:21:40 -0400

    first push

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
-"""
-Main cli or app entry point
-"""
+import polars as pl
+import matplotlib.pyplot as plt
 
-from mylib.calculator import add
-import click
+# Read data into a Polars DataFrame
+df = pl.read_csv('AAPL.csv')
 
-#var=1;var=2
+# print(df.head())
 
-@click.command("add")
-@click.argument("a", type=int)
-@click.argument("b", type=int)
-def add_cli(a, b):
-    click.echo(add(a, b))
+# Calculate summary statistics
+summary_stats = df.describe()
+print(summary_stats)
+
+# Add median row to the summary statistics
+median = df.median()
+
+print(summary_stats, median)
+
+# Create a line plot of Open prices
+plt.plot(df['Open'])
+plt.xticks(rotation=20)
+plt.xlabel('Number of days')
+plt.ylabel('Open Price')
+plt.title('Open price for AAPL in the past year')
+plt.savefig('Open price figure with Polars.png')
+plt.show()
 
 
-if __name__ == "__main__":
-    # pylint: disable=no-value-for-parameter
-    add_cli()
+
+
+
+
